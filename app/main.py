ee2d25cbece2e46717b6c39d514a2d64991990c0
Alex1108002466 <gorodokalexey2006@gmail.com>
2026-06-13 16:43:00 +0300

    Add hardcoded pokemon data

--- app/main.py
+++ app/main.py
@@ -1,8 +1,50 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+ 
 app = FastAPI()
+ 
+class PokemonInfo(BaseModel):
+    name: str
+    base_experience: int
+    height: float
+    type: list[str]
+    weight: float
 
+POKEMONS = {
+    "pikachu": PokemonInfo(
+        name = "pikachu",
+        base_experience = 112,
+        height = 0.4,
+        type = ["electric"],
+        weight = 6.0,
+    ),
+    "charizard": PokemonInfo(
+        name = "charizard",
+        base_experience = 267,
+        height = 1.7,
+        type = ["fire", "flying"],
+        weight = 90.5,
+    ),
+    "mewtwo": PokemonInfo(
+        name = "mewtwo",
+        base_experience = 340,
+        height = 2.0,
+        type = ["psychic"],
+        weight = 122.0,
+    )
+}
+ 
 @app.get("/")
-
 def root():
-    return {"Score": 10}
+    return {"message": "Pokemon API работает!"}
+
+app.get("/pokemon")
+def pokemons():
+    return list(POKEMONS.keys())
+
+@app.get("/pokemon/{name}", response_model = PokemonInfo)
+def get_pokemon(name: str):
+    pokemon = POKEMONS.get(name)
+    if not pokemon:
+        raise HTTPException(status_code = 404, detail = f"Покемон {name} не найден!")
+    return pokemon
